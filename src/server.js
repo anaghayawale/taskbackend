186d0b95bb0ad77370af47128be4296a7f8f3c07
anaghayawale <anaghayawale7@gmail.com>
2023-12-07 11:51:44 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
 const Note = require('./models/Note');
-
 const bodyParser = require('body-parser');
+require('dotenv').config()
+console.log("this is env",process.env.ANAGHA)
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const mongodbPath = 'mongodb+srv://anaghayawale:tonystark123@cluster0.7rm3blw.mongodb.net/notesdb';
+const mongodbPath = process.env.MONGO_URI || '' ;
+
 mongoose.connect(mongodbPath).then(function (){
-    app.get('/', (req, res) => {
-        const response = {success: true, message: "Server is running!"};
-        res.json(response);
-    });
-    
+    console.log("database connected")
     const noteRouter = require('./routes/Note');
     app.use('/api', noteRouter)
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, function (){
     console.log('Server is running on port: ' + PORT);
 });

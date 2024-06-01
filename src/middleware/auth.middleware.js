e3fb40b731ebf7da5af8bf2eb92b5415968165d8
anaghafreny <anaghayawale7@gmail.com>
2024-06-01 14:13:37 +0530

    feat: api error to api response

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,19 +6,13 @@ import { User } from "../models/user.model.js";
 const checkJWT = asyncHandler(async (req, res, next) => {
   const authHeader = req.header("Authorization");
   const accessToken = authHeader ? authHeader.replace("Bearer ", "") : null;
-  // const { accessToken } =
-  //   req.cookies || req.header("Authorization")?.replace("Bearer ", "");
-  const { refreshToken } = req.cookies || req.body?.refreshToken;
-  console.log("accessToken:",accessToken);
-
-  if (!accessToken || !refreshToken) {
+  
+  if (!accessToken) {
     throw new ApiError(401, "Unauthorized");
   }
-
-  const { _id } = accessToken
-    ? jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-    : jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-  console.log("_id:",_id);
+  console.log("accessToken:",accessToken);
+  let { _id } = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+  console.log("id:",_id);
   if (!_id) {
     throw new ApiError(401, "Unauthorized");
   }
